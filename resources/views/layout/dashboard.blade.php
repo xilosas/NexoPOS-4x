5dd5ac714b5782ae1273da03bb87e657023ce010
xilosas <XILOHECKO86@GMAIL.COM>
2025-06-08 23:31:17 +0800

    Update dashboard.blade.php

--- resources/views/layout/dashboard.blade.php
+++ resources/views/layout/dashboard.blade.php
@@ -103,7 +103,7 @@ use App\Services\DateService;
                 </div>
                 <div class="p-2 text-xs flex justify-end text-gray-500">
                     {!! 
-                        Hook::filter( 'ns-footer-signature', sprintf( __( 'You\'re using <a tager="_blank" href="%s" class="hover:text-blue-400 mx-1 inline-block">NexoPOS %s</a>' ), 'https://my.nexopos.com/en', config( 'nexopos.version' ) ) )
+                        Hook::filter( 'ns-footer-signature', sprintf( __( 'You\'re using <a tager="_blank" href="%s" class="hover:text-blue-400 mx-1 inline-block">Metal Scrap %s</a>' ), 'https://pos.kopetrotrading.com.my', config( 'nexopos.version' ) ) )
                     !!}
                 </div>
             </div>
